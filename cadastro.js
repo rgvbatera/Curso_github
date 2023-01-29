13aab544cad991e08c204fde77e0e72c5274a03a
Renan <rgvbatera@outlook.com>
2023-01-29 16:43:27 -0300

    adicionando mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,4 +1,11 @@
 function signup(mail) {
     return email;
 }
+
 //pegando o email e cadastrando na plataforma. 
+
+function mostraMensagem(mail) {
+    return 'Bem vindo ao sistema';
+}
+
+//adicionando mensagem.
